6a6fffb88b9cd846a1bbe149d06ba5ff72171418
Qu Zhe <zqu@thoughtworks.com>
2020-05-21 10:45:28 +0800

    [quzhe] refactor(core): overwrite equals and hashcode for entity

--- src/main/java/com/scene/domain/core/Entity.java
+++ src/main/java/com/scene/domain/core/Entity.java
@@ -5,6 +5,17 @@ public abstract class Entity<T> {
 
     @Override
     public boolean equals(Object obj) {
-        return super.equals(obj);
+        if (obj == this) return true;
+        if (!obj.getClass().equals(getClass())) return false;
+        Entity<T> other = (Entity<T>) obj;
+        return other.getId().equals(getId());
+    }
+
+    @Override
+    public int hashCode() {
+        final int PRIME = 31;
+        int result = 1;
+        result = PRIME * result + ((getId() == null) ? 0 : getId().hashCode());
+        return result;
     }
 }
